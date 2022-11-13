d7feec2cc4b3b887ba0abf0222e795366c9fc348
prathamesh-vaibhav-pawar <107491217+prathamesh-vaibhav-pawar@users.noreply.github.com>
2022-11-13 16:52:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function playerb(){
+    var player1 = document.getElementById("i1").value
+    localStorage.setItem("player1",player1);
+    var player2 = document.getElementById("i2").value
+    localStorage.setItem("player2",player2)
+}
